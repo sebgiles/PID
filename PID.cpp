fcdc1d5a956e6eff4c00a86d1bd475036142e375
sebgiles <mrsebgiles@gmail.com>
2016-11-15 17:03:43 +0100

    old and uncommited

--- PID.cpp
+++ PID.cpp
@@ -1,6 +1,6 @@
 //
-//  NXTServo.cpp
-//  
+//  PID.cpp
+//
 //
 //  Created by Sebastian Giles on 12/12/14.
 //
@@ -15,65 +15,74 @@ kd(kd)
 {
 }
 
+PID::PID(){}
+
 float PID::compute(float target, float input, float time){
-    PIDData data;
-    
-    data.target=target;
-    data.input=input;
-    data.time=time;
-    
-    return compute(data);
+  PIDData data;
+
+  data.target=target;
+  data.input=input;
+  data.time=time;
+
+  return compute(data);
 }
 
 float PID::compute(PIDData& data){
-    
-    if (data.target!=lastTarget){ //when target changes everything is reset
-        firstLoop=true;
-        lastTime=data.time;
-        lastError=0;
-        accumulatedError=0;
-    }
-    
-    float error = data.target - data.input;
-    float dError = error - lastError;
-    lastError = error;
 
-    if (error>=-tolerance && error<=tolerance)
-        error=0;
-    
-    float dTime = data.time - lastTime;
-    lastTime = data.time;
+  if (data.target!=lastTarget){ //when target changes everything is reset
+    firstLoop=true;
+    lastTime=data.time;
+    lastError=0;
+    integral=0;
+    lastTarget=data.target;
+  }
 
-    data.p=kp * error;
-    
-    if (firstLoop) {
-        data.i=0;
-        data.d=0;
-        data.output=data.p;
-        
-        firstLoop=false;
-    } else {
-    
-        //if(dError==0 && error==0) errorIntegral=0;
-        
-        accumulatedError+= (dTime* error);
-        
-        data.i = ki * accumulatedError;
-        data.d = kd * dError/dTime;
-        
-        data.output = data.p + data.i + data.d;
-    }
-    return data.output;
+  float error = data.target - data.input;
+  float dError = error - lastError;
+  lastError = error;
+
+  if (error>=-tolerance && error<=tolerance){
+    error=0;
+    if(dError==0) integral=0;
+  }
+
+
+  float dTime = data.time - lastTime;
+  lastTime = data.time;
+
+  data.p=kp * error;
+
+  if (firstLoop) {
+    data.i=0;
+    data.d=0;
+    data.output=data.p;
+
+    firstLoop=false;
+  } else {
+
+    integral+= dTime * error * ki;
+
+    if (integral > limit) integral = limit;
+    else if (integral < -limit) integral = -limit;
+
+    data.i = integral;
+    data.d = kd * dError/dTime;
+
+    data.output = data.p + data.i + data.d;
+  }
+  return data.output;
 }
 
 void PID::tune(float newkp, float newki, float newkd){
-    kp=newkp;
-    ki=newki;
-    kd=newkd;
+  kp=newkp;
+  ki=newki;
+  kd=newkd;
 }
 
 void PID::tolerate(float newtolerance){
-    tolerance=newtolerance;
+  tolerance=newtolerance;
 }
 
-
+void PID::setMaximumOutput(float maxOutput){
+  limit=maxOutput;
+}
--- PID.h
+++ PID.h
@@ -9,41 +9,48 @@
 #ifndef ____PID__
 #define ____PID__
 
+#define DEFAULT_MAX_OUTPUT 1
+
 struct PIDData{
-    float time, target, input, output, p, i, d;
+  float time, target, input, output, p, i, d;
 };
 
 class PID{
 public:
-    PID(float kp, float ki, float kd);
-    
-    float compute(float target, float input, float time);
-    
-    float compute(PIDData& data);
+  PID();
+  PID(float kp, float ki, float kd);
 
-    void tune(float kp, float ki, float kd);
-    
-    void tolerate(float tolerance);
-    
+  float compute(float target, float input, float time);
+
+  float compute(PIDData& data);
+
+  void tune(float kp, float ki, float kd);
+
+  void tolerate(float tolerance);
+
+  void setMaximumOutput(float maxOutput);
+
 private:
-    
-    float tolerance=0;
-    
-    bool firstLoop = true; //derivative can't be calculated on a single measure
-    
-    float lastTarget;
-    
-    long lastTime;
-    
-    float lastError; //used to compute derivative of error
-    float accumulatedError;
-    
-    //proportional, integral and derivative coefficients
-    float kp;
-    float ki;
-    float kd;
 
-    
+  float limit=DEFAULT_MAX_OUTPUT;
+
+  float tolerance=0;
+
+  bool firstLoop = true; //derivative can't be calculated on a single measure
+
+  float lastTarget;
+
+  long lastTime;
+
+  float lastError; //used to compute derivative of error
+  float integral;
+
+  //proportional, integral and derivative coefficients
+  float kp;
+  float ki;
+  float kd;
+
+
 };
 
 
